5a27502a9a62195b6729e2e829aaa515d750dcb8
Raffaello Di Martino IZ0QWM <42165424+iz0qwm@users.noreply.github.com>
2019-03-27 21:40:27 +0100

    Install script by Joep, PD1AEF

--- install-gp2009tr.sh
+++ install-gp2009tr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -       
 #title           :install-gp2009tr.sh
 #description     :This script will install the gp2009tr python script for you
-#author		 	     :Joep, PD1AEF
+#author          :Joep, PD1AEF
 #date            :20190327
 #version         :0.1   
-#usage		       :bash install-gp2009tr.sh
+#usage           :bash install-gp2009tr.sh
 #notes           :Based on the nice work by Raffaello, IZ0QWM
 
 wget https://github.com/iz0qwm/gp2009tr/archive/master.zip
