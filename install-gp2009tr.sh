6a6d9364b6d91d613e5c8bb04e0a3170ec2300c6
Raffaello Di Martino IZ0QWM <42165424+iz0qwm@users.noreply.github.com>
2019-03-27 21:28:56 +0100

    Install script by Joep, PD1AEF

---
+++ install-gp2009tr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -       
+#title           :install-gp2009tr.sh
+#description     :This script will install the gp2009tr python script for you
+#author		 	 :Joep, PD1AEF
+#date            :20190327
+#version         :0.1   
+#usage		     :bash install-gp2009tr.sh
+#notes           :Based on the nice work by Raffaello, IZ0QWM
+
+wget https://github.com/iz0qwm/gp2009tr/archive/master.zip
+unzip master.zip
+rm master.zip
+cd gp2009tr-master/
+sudo apt install python-pip
+pip install websocket websocket-client configparser 
+
+
